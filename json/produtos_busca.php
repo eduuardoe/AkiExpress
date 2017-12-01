74ef6e64c8886448c0658a712c3e368520654f8d
eduuardoe <eduardo.eleuterio@gmail.com>
2017-12-01 21:43:33 -0200

    Delete produtos_busca.php

--- json/produtos_busca.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $palavra = "%".trim($_GET["palavra"])."%";
-        
-    $sql = "select * from produto "
-            . "where nome like ?";
-
-    $res = $pdo->prepare($sql);
-    $res->bindParam(1, $palavra);
