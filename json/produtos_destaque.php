759fbb7053e43fedf51f6a0b3ca3d59d8658cf57
eduuardoe <eduardo.eleuterio@gmail.com>
2017-12-01 21:43:41 -0200

    Delete produtos_destaque.php

--- json/produtos_destaque.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $sql = "select * from produto "
-                . "where destaque = 'S' "
-                . "order by rand() "
-                . "limit 4";
-        
-    $res = $pdo->prepare($sql);
